2bda8b0c6cbc63a641ee2fcb366294abef97a8d9
turki-aloufi <turkialoufi44@gmail.com>
2024-08-01 18:54:44 +0300

    create employee class

---
+++ employees_manager/bin/employee.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+class Employee {
+  Employee(
+      {required this.name,
+      required this.dateOfBirth,
+      required this.permissions,
+      required this.salary,
+      required this.jobTitle,
+      required this.jobDescription}) {
+    id = Random().nextInt(10000);
+  }
+  Employee.initial(
+      {required this.id,
+      required this.name,
+      required this.dateOfBirth,
+      required this.permissions,
+      required this.salary,
+      required this.jobTitle,
+      required this.jobDescription});
+  late int id;
+  late String name;
+  late String dateOfBirth;
+  late double salary;
+  late List<String> permissions;
+  late String jobTitle;
+  late String jobDescription;
+
+  printInfo() {
+    print("----------My info----------");
+    print("Id: $id");
+    print("Name: $name");
+    print("Date of Birth: $dateOfBirth");
+    print("Salary: $salary");
+    print("Permissions: $permissions");
+    print("Job title: $jobTitle");
+    print("JobDescription: $jobDescription");
+  }
+}
--- employees_manager/bin/employees_manager.dart
+++ employees_manager/bin/employees_manager.dart
@@ -1,5 +1,3 @@
-import 'package:employees_manager/employees_manager.dart' as employees_manager;
-
-void main(List<String> arguments) {
-  print('Hello world: ${employees_manager.calculate()}!');
+void main() {
+  
 }
