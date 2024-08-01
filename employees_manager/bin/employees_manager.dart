d7fdfbca74df60bd38cbfc1d70fd73ddd43998f6
turki-aloufi <turkialoufi44@gmail.com>
2024-08-01 17:58:40 +0300

    initial commit

---
+++ employees_manager/bin/employees_manager.dart
@@ -0,0 +1,5 @@
+import 'package:employees_manager/employees_manager.dart' as employees_manager;
+
+void main(List<String> arguments) {
+  print('Hello world: ${employees_manager.calculate()}!');
+}
---
+++ employees_manager/test/employees_manager_test.dart
@@ -0,0 +1,8 @@
+import 'package:employees_manager/employees_manager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
